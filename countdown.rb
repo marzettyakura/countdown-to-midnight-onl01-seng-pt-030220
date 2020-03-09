6f2d1a563bb9483ec73da3b6118328f3761deba6
pleasant-archiver-6104 <marzettyakura@yahoo.com>
2020-03-09 05:26:41 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,29 @@
-#write your code here
+## while seconds is greater than 0
+## print "#{seconds} SECOND(S)"
+## and decrement by 1
 
-def countdown
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+  seconds -= 1
 end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+ while seconds > 0
+   puts "#{seconds} SECOND (S)!"
+   sleep 1
+   seconds -= 1
+
+ end
+ return "HAPPY NEW YEAR!"
+ end
+
+
+
+
+
+
+
+##
