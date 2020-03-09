c7c2b43735337c2eabe3e08ef768874f2c9c525e
pleasant-archiver-6104 <marzettyakura@yahoo.com>
2020-03-09 05:17:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
+## while seconds is greater than 0
+## print "#{seconds} SECOND(S)"
+## and decrement by 1
 
 def countdown
+  while seconds > 0
+    puts "#{seconds} SECOND(S)"
+  seconds -= 1
+end
+return "HAPPY NEW YEAR!"
 end
