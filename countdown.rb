6b51d2dc2b42f43d1b52405dadcdad96ba6e46d2
pleasant-archiver-6104 <marzettyakura@yahoo.com>
2020-03-09 05:25:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@
 
 def countdown(seconds)
   while seconds > 0
-    puts "#{seconds} SECOND(S)"
+    puts "#{seconds} SECOND(S)!"
   seconds -= 1
 end
 return "HAPPY NEW YEAR!"
@@ -12,7 +12,7 @@ end
 
 def countdown_with_sleep(seconds)
  while seconds > 0
-   puts "#{seconds} SECOND (S)"
+   puts "#{seconds} SECOND (S)!"
    sleep 1
    seconds -= 1
 
